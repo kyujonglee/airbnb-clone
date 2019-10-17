95305179f9a571a0aad87f0c3ac07c11d401f1a5
이규종 <kyujong93@naver.com>
2019-10-17 22:06:00 +0900

    [Add] 네이버 로그인 로직작성
    서버에서 쿠키에 보내온 jwt토큰을 가지고 로그인 로직을 실행시킨 다음에
    해당 쿠키를 삭제함.

--- front/src/routes/Auth/AuthContainer.js
+++ front/src/routes/Auth/AuthContainer.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthPresenter from './AuthPresenter';
 import { useMutation } from '@apollo/react-hooks';
 import useInput from '../../hooks/useInput';
 import { REQUEST_TOKEN, LOGIN_QUERY } from './AuthQueries';
 import { toast } from 'react-toastify';
+import { parseCookies } from '../../util';
 
 const AuthContainer = () => {
   const [action, setAction] = useState('login');
   const email = useInput('');
   const password = useInput('');
-  const [token, setToken] = useState('');
 
   const [createTokenMutation] = useMutation(REQUEST_TOKEN, {
     variables: {
@@ -18,7 +18,7 @@ const AuthContainer = () => {
     }
   });
 
-  const [loginMutation] = useMutation(LOGIN_QUERY, { variables: { token } });
+  const [loginMutation] = useMutation(LOGIN_QUERY);
 
   const login = async () => {
     if (email !== '' && password !== '') {
@@ -26,8 +26,7 @@ const AuthContainer = () => {
         const {
           data: { createToken: token }
         } = await createTokenMutation();
-        setToken(token);
-        await loginMutation();
+        await loginMutation({ variables: { token } });
         toast('로그인 되었습니다.');
       } catch (error) {
         toast.error('로그인 실패: 아이디와 비밀번호를 확인해주세요.');
@@ -54,8 +53,28 @@ const AuthContainer = () => {
   };
 
   const naverLogin = () => {
-    
-  }
+    window.location = 'http://localhost:4000/auth/naver';
+  };
+
+  const socialLogin = async () => {
+    try {
+      let cookies = document.cookie;
+      cookies = parseCookies(cookies);
+      const token = cookies['token'].trim();
+      if (token) {
+        await loginMutation({ variables: { token } });
+        toast('로그인 되었습니다.');
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    socialLogin();
+    return () => {
+      document.cookie = 'token=; expires=Thu, 18 Dec 2013 12:00:00 UTC';
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <AuthPresenter
